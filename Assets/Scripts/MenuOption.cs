63678e59ed61b6f0259d25bb988826f19019a071
Pedro Henrique Cascalho E Silva <116205652+PedroCascalho@users.noreply.github.com>
2023-04-24 21:16:56 -0300

    Fazendo script da seta do menu

--- Assets/Scripts/MenuOption.cs
+++ Assets/Scripts/MenuOption.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuOption : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public RectTransform arrowTransform; // Transform da imagem da seta
+    public RectTransform newGameTransform; // Transform da opção "New Game"
+    public RectTransform continueTransform; // Transform da opção "Continue"
 
-    // Update is called once per frame
-    void Update()
+    public float arrowMoveSpeed = 10f; // Velocidade de movimento da seta
+
+    private void Update()
     {
-        
+        // Movimento da seta com as setas do teclado
+        float verticalInput = Input.GetAxis("Vertical");
+        if (verticalInput > 0) // Setor para cima
+        {
+            arrowTransform.position = Vector3.MoveTowards(arrowTransform.position, newGameTransform.position, arrowMoveSpeed * Time.deltaTime);
+        }
+        else if (verticalInput < 0) // Setor para baixo
+        {
+            arrowTransform.position = Vector3.MoveTowards(arrowTransform.position, continueTransform.position, arrowMoveSpeed * Time.deltaTime);
+        }
+
+        // Movimento da seta com o mouse
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (newGameTransform.rect.Contains(mousePosition)) // Se o mouse estiver sobre a opção "New Game"
+        {
+            arrowTransform.position = Vector3.MoveTowards(arrowTransform.position, newGameTransform.position, arrowMoveSpeed * Time.deltaTime);
+        }
+        else if (continueTransform.rect.Contains(mousePosition)) // Se o mouse estiver sobre a opção "Continue"
+        {
+            arrowTransform.position = Vector3.MoveTowards(arrowTransform.position, continueTransform.position, arrowMoveSpeed * Time.deltaTime);
+        }
     }
 }
